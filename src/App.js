b754526d2673fc896c6bd8e4921bd2849eba874d
henidahmen <henidahmen789@gmail.com>
2021-05-04 10:08:06 +0100

    jsx

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from "./imgInSrc.jpg"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+          <h1 className="title red">henidahmen</h1>
+          
+          <img src="/imgInPublic.jpg" alt=""/>
+          <img src={logo} alt="logo"/>
+          
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+    </div>
+  );
+}
+
+export default App;
